48d5e5221aa68bc0fc3441e3f1a2a86f68b15a94
seungjinhan <hanblues@gmail.com>
2023-04-17 18:12:23 +0900

    code정리

--- src/log/log.service.ts
+++ src/log/log.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@nestjs/common';
 import { CreateLogDto } from './dto/create-log.dto';
-import { UpdateLogDto } from './dto/update-log.dto';
 import { PrismaService } from 'src/config/core/prisma/prisma.service';
 import { LogEntity as LogEntity } from './entities/log.entity';
 
